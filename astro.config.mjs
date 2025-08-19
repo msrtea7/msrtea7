10301d64659ad484e89b1dec089af0f241c89e1f
msrtea7_desktop <msrtea7@gmail.com>
2025-08-19 10:43:23 +0200

    new way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 
 
 import icon from 'astro-icon';
@@ -9,7 +10,15 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://msrtea7.com',
-    integrations: [mdx(), icon()],
+    integrations: [
+        mdx(), 
+        icon(), 
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
     markdown: {
         shikiConfig: {
             theme: 'gruvbox-light-hard',
